88a44d5f34a5b28dd164d828606d56b40f5fa35f
Samad <sam.an.vns6@gmail.com>
2026-02-09 17:28:44 +0530

    supabase storage

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 import '../auth/login_screen.dart';
@@ -12,44 +15,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   final supabase = Supabase.instance.client;
-  RealtimeChannel? channel;
-  final msgController = TextEditingController();
-  List<Map<String, dynamic>> messages = [];
-
-  listenBroadcast() {
-    channel = supabase.channel(
-      'room1',
-      opts: RealtimeChannelConfig(self: true),
-    );
-    channel?.onBroadcast(
-      event: 'event1',
-      callback: (payload) {
-        debugPrint('Payload: $payload');
-        setState(() {
-          messages.add(payload);
-        });
-      },
-    );
-    channel?.subscribe();
-  }
-
-  sendMessage() async {
-    String text = msgController.text;
-    msgController.clear();
-
-    ChannelResponse? response = await channel?.sendBroadcastMessage(
-      event: 'event1',
-      payload: {'text': text, 'user_id': supabase.auth.currentUser?.id},
-    );
-
-    debugPrint('Message status: ${response?.name}');
-  }
-
-  @override
-  void initState() {
-    listenBroadcast();
-    super.initState();
-  }
+  File? pickedFile;
 
   @override
   Widget build(BuildContext context) {
@@ -74,61 +40,27 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ],
       ),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView(
-              children: [
-                for (var msg in messages)
-                  Align(
-                    alignment: msg['user_id'] == supabase.auth.currentUser?.id
-                        ? Alignment.centerRight
-                        : Alignment.centerLeft,
-                    child: Container(
-                      constraints: BoxConstraints(
-                        maxWidth: 300
-                      ),
-                      margin: EdgeInsets.all(4),
-                      padding: EdgeInsets.symmetric(horizontal: 12, vertical: 4),
-                      decoration: BoxDecoration(
-                        color: msg['user_id'] == supabase.auth.currentUser?.id
-                            ? Colors.blue
-                            : Colors.grey.shade300,
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Text(
-                        msg['text'],
-                        style: TextStyle(
-                          color: msg['user_id'] == supabase.auth.currentUser?.id
-                              ? Colors.white
-                              : Colors.black,
-                        ),
-                      ),
-                    ),
-                  ),
-              ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            pickedFile == null ? SizedBox() :
+            Image.file(pickedFile!),
+            ElevatedButton(
+              onPressed: () async {
+                final result = await ImagePicker().pickImage(
+                  source: ImageSource.gallery,
+                );
+                if (result != null) {
+                  setState(() {
+                    pickedFile = File(result.path);
+                  });
+                }
+              },
+              child: Text("Pick File"),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-            child: Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    controller: msgController,
-                    decoration: InputDecoration(
-                      hintText: "Type your messages...",
-                    ),
-                  ),
-                ),
-                IconButton(
-                  onPressed: () => sendMessage(),
-                  icon: Icon(Icons.send, color: Colors.blue),
-                ),
-              ],
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
